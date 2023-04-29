51c9c4db3addef64c0335c96605d69bb276619cc
valek <valekgodovan@gmail.com>
2023-04-29 15:05:56 +0600

    add articles

---
+++ hexlet_django_blog/article/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+articles = [
+    {'id': 1, 'title': '"How to foo?"', 'author': 'F. BarBaz'},
+    {'id': 2, 'title': '"Force 101"', 'author': 'O-W. Kenobi'},
+    {'id': 3, 'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
+    {'id': 4, 'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
+    {'id': 5, 'title': '"5 min recepies"', 'author': 'H. Lector'},
+]
+
+def index(request):
+    return render(request, 'articles/index.html', context={'data': articles})
